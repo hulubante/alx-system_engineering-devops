39718d36500c3454ff90281d2061867d3bcfef5e
Hulubante <hulubante.a@gmail.com>
2022-06-02 13:27:04 -0700

    commit printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *main - print the phrase "programming is like building a multilingual puzzle 
- *return: 0 if exited properly, non-zero otherwise
- */
+*main-print the phrase"programming is like building a multilingual puzzle 
+*return:0 if exited properly, non-zero otherwise
+*/
 int main(void)
 {
- puts("programming is like building a multilingual puzzle");
- return(0);
+puts("programming is like building a multilingual puzzle");
+return(0);
 }
