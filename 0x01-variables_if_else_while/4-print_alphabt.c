44e3313cee66e38230300205ff0f38b65169c7f8
Hulubante <hulubante.a@gmail.com>
2022-06-03 15:09:27 -0700

    commmit 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
  * main - prints the alphabet is lowercase
@@ -11,7 +11,7 @@ int main(void)
 
 	while (ch <= 'z')
 	{
-		if (ch!= 'e' && ch != 'q')
+		if (ch != 'e' && ch != 'q')
 		{
 		 putchar(ch);
 		}
