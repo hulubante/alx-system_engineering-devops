24bd34f4a70d4cd35c37e1915eedb8ceaff67370
Hulubante <hulubante.a@gmail.com>
2022-06-06 06:17:03 -0700

    commit comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
  * main - prints all possible combination of two
@@ -14,10 +14,10 @@ int main(void)
 		{
 			if (i < j)
 			{
-				putchar((i/10) + 48);
+				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar((j/10) + 48);
+				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
 				if (i != 98 || j != 99)
 				{
