4eb43dedf7a8406ea20123742552164e246d8a5e
Hulubante <hulubante.a@gmail.com>
2022-06-06 03:51:42 -0700

    commit comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,13 +14,13 @@ int main(void)
 		  {
 			  putchar((digit1 % 10) + '0');
 			  putchar((digit2 % 10) + '0');
-
 			  if (digit1 == 9 && digit2 == 9)
 				  continue;
-
+			  {
 				  putchar(',');
 				  putchar(' '); 
 			  }
+		  }
 		  }
 
 	putchar('\n');
