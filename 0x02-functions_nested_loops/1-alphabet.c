70550227c2b555ff6651abdb4b7c178f1407a4a4
Hulubante <hulubante.a@gmail.com>
2022-06-07 00:28:58 -0700

    commit alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,20 @@
+#include "_putchar.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch='a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
 
