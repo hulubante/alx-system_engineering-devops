92dba9393632f740b422cc30be028543b04dbd33
Hulubante <hulubante.a@gmail.com>
2022-06-08 12:06:44 -0700

    commit 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,14 +3,17 @@
 
 /**
  * print_to_98 - Prints sequence of integers from argument value to 98
- * Description: Modify value of 'target' to alter number to print to
+ * Description: Modify value oe 'target' to alter number to print to
  * @n: First value to be printed in sequence to 98
  * Return: None
  */
+
 void print_to_98(int n)
 {
 	int i, target, comma = 0;
+
 	target = 98;  /* Change this value to adjust number to print to */
+
 	if (n <= target)
 	{
 		for (i = n; i <= target; i++)
