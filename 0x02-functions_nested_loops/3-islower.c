c38bd869417570982df9e0645b7b56f885130786
Hulubante <hulubante.a@gmail.com>
2022-06-08 01:17:55 -0700

    commit 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,7 +5,14 @@
  * @c: the character to check
  * Return: 1 if c is lowercase, 0 otherwise
  */
-int_islower(innt c)
+int_islower(int c)
 {
-	return (c >= 'a' && c <= 'z');
+	if  (c >= 'a' && c <= 'z');
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
