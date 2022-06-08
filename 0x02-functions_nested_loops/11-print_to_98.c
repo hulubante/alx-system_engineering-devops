cf78b07030b67a96bd072b908b11a088f50d0970
Hulubante <hulubante.a@gmail.com>
2022-06-08 06:54:56 -0700

    commit 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - computes and print the sum of all the multiples
+ * of 3 or below 1024
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	unsigned long int sum3, sum5, sum;
+	int i;
+
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; ++i)
+	{
+		if ((i % 3) == 0)
+		{
+			sum3 = sum 3
