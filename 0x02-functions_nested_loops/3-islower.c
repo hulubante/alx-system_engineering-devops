43589fc85ad22cd315388a73199e899f1523cbbd
Hulubante <hulubante.a@gmail.com>
2022-06-08 05:03:33 -0700

    commit 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- *_islower - returns 1 if lowercase alphabet and 0 otherwise
+ * _islower - returns 1 if lowercase alphabet and 0 otherwise
  * @c: takes in a character
- * Return:0 for uppercase, 1 for lowercase
+ * Return: 0 for uppercase, 1 for lowercase
  */
 
 int _islower(int c)
 {
-	if  (c >= 'a' && c <= 'z');
+	if  (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
