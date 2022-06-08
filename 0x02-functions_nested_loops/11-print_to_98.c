4acdc261e5d757e85ba4bc34283983474ab1115c
Hulubante <hulubante.a@gmail.com>
2022-06-08 11:56:49 -0700

    commit 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,30 +1,31 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - computes and print the sum of all the multiples
- * of 3 or below 1024
- * Return: Always 0 (success)
+ * print_to_98 - Prints sequence of integers from argument value to 98
+ * Description: Modify value of 'target' to alter number to print to
+ * @n: First value to be printed in sequence to 98
+ * Return: None
  */
-int main(void)
+void print_to_98(int n)
 {
-	unsigned long int sum3, sum5, sum;
-	int i;
-
-	sum3 = 0;
-	sum5 = 0;
-	sum = 0;
-
-	for (i = 0; i < 1024; ++i)
+	int i, target, comma = 0;
+	target = 98;  /* Change this value to adjust number to print to */
+	if (n <= target)
 	{
-		if ((i % 3) == 0)
+		for (i = n; i <= target; i++)
 		{
-			sum3 = sum3 + i;
-		} else if ((i % 5) == 0)
-		{
-			sum5 = sum5 + i;
+			comma == 0 ? comma = 1 : printf(", ");
+			printf("%d", i);
 		}
 	}
-	sum = sum3 + sum5;
-	printf("%lu\n", sum);
-	return (0);
+	else
+	{
+	for (i = n; i >= target; i--)
+	{
+		comma == 0 ? comma = 1 : printf(", ");
+		printf("%d", i);
+	}
+	}
+	putchar('\n');
 }
+
